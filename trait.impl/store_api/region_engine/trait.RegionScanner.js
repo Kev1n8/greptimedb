0112357f26f0dc81121801cdb847839d5b340e52
Kev1n8 <100595273+Kev1n8@users.noreply.github.com>
2024-10-22 15:27:40 +0000

    Deploying to gh-pages from @ Kev1n8/greptimedb@0f752717db51afd8d2405e88ef834e55faaa6385 🚀

---
+++ trait.impl/store_api/region_engine/trait.RegionScanner.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"mito2":[["impl <a class=\"trait\" href=\"store_api/region_engine/trait.RegionScanner.html\" title=\"trait store_api::region_engine::RegionScanner\">RegionScanner</a> for <a class=\"struct\" href=\"mito2/read/seq_scan/struct.SeqScan.html\" title=\"struct mito2::read::seq_scan::SeqScan\">SeqScan</a>"],["impl <a class=\"trait\" href=\"store_api/region_engine/trait.RegionScanner.html\" title=\"trait store_api::region_engine::RegionScanner\">RegionScanner</a> for <a class=\"struct\" href=\"mito2/read/unordered_scan/struct.UnorderedScan.html\" title=\"struct mito2::read::unordered_scan::UnorderedScan\">UnorderedScan</a>"]],
+"store_api":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
